5184d84b30878313ac0e11d5d190a1baf5008654
Bryson Matsuda <han3wf@virginia.edu>
2024-04-09 22:23:59 -0400

    implemented shuffling

--- Controller.js
+++ Controller.js
@@ -7,14 +7,28 @@ class Controller{
         this.displayWords(newCategories);
     }
 
-    displayWords(categoriesData) {
+    displayWords(categoriesData){
         var table = document.getElementById("categoriesTable");
         table.innerHTML = "";
-        if (!categoriesData || !Array.isArray(categoriesData.categories)) {
-            console.error('Invalid categories data:', categoriesData);
-            return;
-        }
         let categories = categoriesData.categories;
+        this.categories = categories;
+        let allWords = categories.flatMap(category => category.words);
+        allWords.sort(() => Math.random() - 0.5);
+        for (let i = 0; i < 4; i++) {
+            let row = table.insertRow();
+            for (let j = 0; j < 4; j++) {
+                let index = i * 4 + j;
+                let word = allWords[index];
+                let cell = row.insertCell();
+                cell.textContent = word;
+            }
+        }
+    }
+
+    handleShuffle(){
+        var table = document.getElementById("categoriesTable");
+        table.innerHTML = "";
+        let categories = this.categories;
         let allWords = categories.flatMap(category => category.words);
         allWords.sort(() => Math.random() - 0.5);
         for (let i = 0; i < 4; i++) {
